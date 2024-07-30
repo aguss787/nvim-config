cc74b3e5b2966c8a80ada117c33c580a5933eb4b
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-30 21:21:36 +0800

    feat(mappings): add window management mappings

--- lua/agus/mapping.lua
+++ lua/agus/mapping.lua
@@ -2,6 +2,13 @@
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
 -- Window management
+vim.keymap.set("n", "<leader>we", "<C-w>=", { desc = "Equalize windows" })
+vim.keymap.set("n", "<leader>wj", "<C-w>-", { desc = "Decrease window height" })
+vim.keymap.set("n", "<leader>wk", "<C-w>+", { desc = "Decrease window height" })
+vim.keymap.set("n", "<leader>wh", "<C-w><", { desc = "Decrease window height" })
+vim.keymap.set("n", "<leader>wl", "<C-w>>", { desc = "Decrease window height" })
+
+-- Split management
 vim.keymap.set("n", "<leader>sv", "<cmd>vsp<CR>", { desc = "Split vertically" })
 vim.keymap.set("n", "<leader>sh", "<cmd>sp<CR>", { desc = "Split horizontally" })
 
