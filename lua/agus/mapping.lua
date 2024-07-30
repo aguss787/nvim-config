610d61672292ee3aeee9899cc3c2ae556d314bc2
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-30 21:03:37 +0800

    docs(mappings): add description on mappings

--- lua/agus/mapping.lua
+++ lua/agus/mapping.lua
@@ -1,33 +1,32 @@
 -- Exit insert mode
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
 -- Window management
-vim.keymap.set('n', '<leader>sv', '<cmd>vsp<CR>', {})
-vim.keymap.set('n', '<leader>sh', '<cmd>sp<CR>', {})
+vim.keymap.set("n", "<leader>sv", "<cmd>vsp<CR>", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>sh", "<cmd>sp<CR>", { desc = "Split horizontally" })
 
 -- Remove highlight until next searcg
-vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>noh<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>/", "<cmd>noh<CR>", { desc = "Remove highlight until next search" })
 
 -- Quickfix
-vim.api.nvim_set_keymap('n', '<leader>qq', '<cmd>copen<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qc', '<cmd>cclose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qf', '<cmd>cfirst<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ql', '<cmd>clast<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qn', '<cmd>cnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qp', '<cmd>cprev<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>qq", "<cmd>copen<CR>", { desc = "Open quickfix" })
+vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>", { desc = "Close quickfix" })
+vim.keymap.set("n", "<leader>qf", "<cmd>cfirst<CR>", { desc = "Go to first quickfix" })
+vim.keymap.set("n", "<leader>ql", "<cmd>clast<CR>", { desc = "Go to last quickfix" })
+vim.keymap.set("n", "<leader>qn", "<cmd>cnext<CR>", { desc = "Go to next quickfix" })
+vim.keymap.set("n", "<leader>qp", "<cmd>cprev<CR>", { desc = "Go to previous quickfix" })
 
 -- LSP related configs
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-vim.keymap.set('n', '<leader>ii', vim.lsp.buf.code_action, {})
-vim.keymap.set('v', '<leader>ii', vim.lsp.buf.code_action, {})
-vim.keymap.set('n', '<leader>ir', vim.lsp.buf.rename, {})
-vim.keymap.set('n', '<leader>iq', vim.diagnostic.setqflist, {})
-vim.keymap.set('n', '<leader>il', vim.diagnostic.setloclist, {})
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "<leader>ii", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("v", "<leader>ii", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>ir", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>iq", vim.diagnostic.setqflist, { desc = "Set quickfix list from diagnostics" })
+vim.keymap.set("n", "<leader>il", vim.diagnostic.setloclist, { desc = "Set location list from diagnostics" })
 
 -- XdgOpenLog
-local xdg_open_log = require('agus.functions.xdg-open-log')
-vim.keymap.set('n', '<leader>x', xdg_open_log.xdgOpenLog, {})
-
+local xdg_open_log = require("agus.functions.xdg-open-log")
+vim.keymap.set("n", "<leader>x", xdg_open_log.xdgOpenLog, { desc = "Show last entry of xdg-open.log" })
