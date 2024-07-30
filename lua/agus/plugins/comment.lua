a069b6ff6536bcdabb70ea3c2d2b7ec795e26434
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-30 21:25:13 +0800

    docs(comment): add description on comment key mapping

--- lua/agus/plugins/comment.lua
+++ lua/agus/plugins/comment.lua
@@ -9,21 +9,26 @@ return {
 		})
 
 		local comment = require("Comment.api")
-		vim.keymap.set("n", "<leader>cc", comment.toggle.linewise.current)
-		vim.keymap.set("n", "<leader>c", comment.call("toggle.linewise", "g@"), { expr = true })
+		vim.keymap.set("n", "<leader>cc", comment.toggle.linewise.current, { desc = "Comment line" })
+		vim.keymap.set("n", "<leader>c", comment.call("toggle.linewise", "g@"), { expr = true, desc = "Comment line" })
 
 		vim.keymap.set("n", "<leader>cbb", comment.toggle.blockwise.current)
-		vim.keymap.set("n", "<leader>cb", comment.call("toggle.blockwise", "g@"), { expr = true })
+		vim.keymap.set(
+			"n",
+			"<leader>cb",
+			comment.call("toggle.blockwise", "g@"),
+			{ expr = true, desc = "Comment block" }
+		)
 
 		local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
 
 		vim.keymap.set("x", "<leader>cc", function()
 			vim.api.nvim_feedkeys(esc, "nx", false)
 			comment.toggle.linewise(vim.fn.visualmode())
-		end)
+		end, { desc = "Comment line" })
 		vim.keymap.set("x", "<leader>cb", function()
 			vim.api.nvim_feedkeys(esc, "nx", false)
 			comment.toggle.blockwise(vim.fn.visualmode())
-		end)
+		end, { desc = "Comment block" })
 	end,
 }
