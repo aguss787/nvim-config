2ff93eb3b04db23e8729ce215875b8523aeda4ed
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-30 22:39:30 +0800

    feat(plugins): add mason-tool-installer

---
+++ lua/agus/plugins/lsp/mason-tool-installer.lua
@@ -0,0 +1,12 @@
+return {
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
+	opts = {
+		ensure_installed = {
+			"stylua",
+			"prettier",
+		},
+	},
+}
