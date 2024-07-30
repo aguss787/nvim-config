65fb8fae5de99e129c65bedc45e9df795fcb83f5
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-30 22:15:15 +0800

    refactor(mappings): move lsp related mappings to LspAttach event

--- lua/agus/lsp.lua
+++ lua/agus/lsp.lua
@@ -1,4 +1,39 @@
-if vim.lsp.inlay_hint then 
-	vim.lsp.inlay_hint.enable(true, { 0 })
-end
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("LspMappings", {}),
+	callback = function(ev)
+		-- enable inlay hints
+		if vim.lsp.inlay_hint then
+			vim.lsp.inlay_hint.enable(true, { ev.buf })
+		end
 
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Go to declaration" })
+		vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { buffer = ev.buf, desc = "Go to definition" })
+		vim.keymap.set(
+			"n",
+			"gi",
+			"<cmd>Telescope lsp_implementations<CR>",
+			{ buffer = ev.buf, desc = "Go to implementation" }
+		)
+		vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", { buffer = ev.buf, desc = "Go to references" })
+
+		vim.keymap.set("n", "<leader>ii", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Code action" })
+		vim.keymap.set("v", "<leader>ii", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Code action" })
+		vim.keymap.set("n", "<leader>ir", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename" })
+
+		-- diagnostics
+		vim.keymap.set(
+			"n",
+			"<leader>dq",
+			vim.diagnostic.setqflist,
+			{ buffer = ev.buf, desc = "Set quickfix list from diagnostics" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>dl",
+			vim.diagnostic.setloclist,
+			{ buffer = ev.buf, desc = "Set location list from diagnostics" }
+		)
+		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { buffer = ev.buf, desc = "Go to next diagnostic" })
+		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_next, { buffer = ev.buf, desc = "Go to next diagnostic" })
+	end,
+})
--- lua/agus/mapping.lua
+++ lua/agus/mapping.lua
@@ -23,17 +23,6 @@ vim.keymap.set("n", "<leader>ql", "<cmd>clast<CR>", { desc = "Go to last quickfi
 vim.keymap.set("n", "<leader>qn", "<cmd>cnext<CR>", { desc = "Go to next quickfix" })
 vim.keymap.set("n", "<leader>qp", "<cmd>cprev<CR>", { desc = "Go to previous quickfix" })
 
--- LSP related configs
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
-vim.keymap.set("n", "<leader>ii", vim.lsp.buf.code_action, { desc = "Code action" })
-vim.keymap.set("v", "<leader>ii", vim.lsp.buf.code_action, { desc = "Code action" })
-vim.keymap.set("n", "<leader>ir", vim.lsp.buf.rename, { desc = "Rename" })
-vim.keymap.set("n", "<leader>iq", vim.diagnostic.setqflist, { desc = "Set quickfix list from diagnostics" })
-vim.keymap.set("n", "<leader>il", vim.diagnostic.setloclist, { desc = "Set location list from diagnostics" })
-
 -- XdgOpenLog
 local xdg_open_log = require("agus.functions.xdg-open-log")
 vim.keymap.set("n", "<leader>x", xdg_open_log.xdgOpenLog, { desc = "Show last entry of xdg-open.log" })
