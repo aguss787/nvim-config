03ab277480095908ac8544b7305cd11e40ae089a
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-30 22:35:07 +0800

    docs(markdown): add description on markdown mapping

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,11 +1,10 @@
 -- Markdown preview
-vim.keymap.set('n', '<leader>mp', function() 
-	vim.cmd('MarkdownPreview') 
-	print('Markdown preview started')
-end, {})
+vim.keymap.set("n", "<leader>mp", function()
+	vim.cmd("MarkdownPreview")
+	print("Markdown preview started")
+end, { desc = "Start markdown preview" })
 
-vim.keymap.set('n', '<leader>ms', function() 
-	vim.cmd('MarkdownPreviewStop') 
-	print('Markdown preview stopped')
-end, {})
-
+vim.keymap.set("n", "<leader>ms", function()
+	vim.cmd("MarkdownPreviewStop")
+	print("Markdown preview stopped")
+end, { desc = "Stop markdown preview" })
