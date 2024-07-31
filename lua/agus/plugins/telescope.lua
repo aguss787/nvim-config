9c50f9a6298ba79975562d4c1adc63095aad2656
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-07-31 21:57:30 +0800

    feat(telescope): use fzf for fuzzy search

--- lua/agus/plugins/telescope.lua
+++ lua/agus/plugins/telescope.lua
@@ -1,20 +1,39 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {
-		defaults = {
-			file_ignore_patterns = {
-				".git/",
-				"target/",
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-					no_ignore = true,
-				},
-			},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
 		},
 	},
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					".git/",
+					"target/",
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						no_ignore = true,
+					},
+				},
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
+	end,
 	keys = {
 		{
 			"<leader>ff",
