ad770903af64f96fba44d76b419ac57920f10e07
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-08-02 15:19:54 +0800

    feat(rustaceanvim): disable clippy to improve DX

--- lua/agus/plugins/lsp/rustacean.lua
+++ lua/agus/plugins/lsp/rustacean.lua
@@ -1,5 +1,25 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
+	"mrcjkb/rustaceanvim",
+	version = "^5", -- Recommended
+	lazy = false, -- This plugin is already lazy
+	config = function()
+		vim.g.rustaceanvim = {
+			-- Plugin configuration
+			tools = {},
+			-- LSP configuration
+			server = {
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						-- disable clippy
+						checkOnSave = {
+							command = "check",
+						},
+					},
+				},
+			},
+			-- DAP configuration
+			dap = {},
+		}
+	end,
 }
