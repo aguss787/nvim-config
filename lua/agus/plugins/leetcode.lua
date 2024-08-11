5d13a8daaba683d468eea7f4772a885cc9c92f38
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-08-11 18:25:10 +0800

    fix(plugins): disable notify

--- lua/agus/plugins/leetcode.lua
+++ lua/agus/plugins/leetcode.lua
@@ -3,12 +3,10 @@ return {
 	build = ":TSUpdate html",
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
-		"nvim-lua/plenary.nvim", -- required by telescope
 		"MunifTanjim/nui.nvim",
 
 		-- optional
 		"nvim-treesitter/nvim-treesitter",
-		"rcarriga/nvim-notify",
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
