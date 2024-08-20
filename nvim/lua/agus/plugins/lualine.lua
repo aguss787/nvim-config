444e66a406c3be5c9c769a263a184687a4fd08bf
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-08-20 19:38:40 +0800

    feat(nvim): add macro view for lualine

--- nvim/lua/agus/plugins/lualine.lua
+++ nvim/lua/agus/plugins/lualine.lua
@@ -2,11 +2,13 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
 		"AndreM222/copilot-lualine",
+		"yavorski/lualine-macro-recording.nvim",
 	},
 	opts = {
 		theme = "catppuccin",
 		sections = {
 			lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
+			lualine_c = { "macro_recording", "%S" },
 		},
 	},
 }
