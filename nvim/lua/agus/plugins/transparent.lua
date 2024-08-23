4f718072ec66bdc072a5289cc986b4cfb99bb713
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-08-23 10:25:44 +0800

    feat(nvim): enable transparent iff TERMINAL_TRANSPARENT env variable is set to TRUE

--- nvim/lua/agus/plugins/transparent.lua
+++ nvim/lua/agus/plugins/transparent.lua
@@ -1,5 +1,8 @@
 return {
 	"tribela/vim-transparent",
+	enabled = function()
+		return os.getenv("TERMINAL_TRANSPARENT") == "TRUE"
+	end,
 	priority = 1000,
 	config = function()
 		vim.g.transparent_groups_add = {
