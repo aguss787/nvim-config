db5841c4d98419e158100970c1a394dbec40260e
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-08-28 23:22:32 +0800

    feat(nvim): show filename on active window

--- nvim/lua/agus/plugins/lualine.lua
+++ nvim/lua/agus/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
 		theme = "catppuccin",
 		sections = {
 			lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
-			lualine_c = { "macro_recording", "%S" },
+			lualine_c = { "filename", "macro_recording" },
 		},
 	},
 }
