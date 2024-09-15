3ba597e9ff702b68636cc2678af6d317154e4e94
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2024-09-15 15:55:55 +0800

    style(nvim): reformat xdgOpenLog

--- nvim/lua/agus/functions/xdg-open-log.lua
+++ nvim/lua/agus/functions/xdg-open-log.lua
@@ -1,39 +1,31 @@
 local M = {}
 
-M.xdgOpenLog = (function(n)
-		local n = n or 1
+M.xdgOpenLog = function(maybeN)
+	local n = maybeN or 1
 
-		local home = os.getenv('HOME')
-		local output = vim.system(
-			{ 'tail', '-n', n, home .. '/.xdg-open.log' },
-			{ text = true }
-		):wait()
+	local home = os.getenv("HOME")
+	local output = vim.system({ "tail", "-n", n, home .. "/.xdg-open.log" }, { text = true }):wait()
 
-		local stdout = output.stdout:match( "^%s*(.-)%s*$" )
-		if stdout == '' then
-			stdout = nil
-		end
-
-		local stderr = output.stderr:match( "^%s*(.-)%s*$" )
-		print(stdout or stderr)
+	local stdout = output.stdout:match("^%s*(.-)%s*$")
+	if stdout == "" then
+		stdout = nil
 	end
-)
 
-vim.api.nvim_create_user_command(
-	'XdgOpenLog',
-	function(opts)	  
-		local opts = opts or {}
-		local args = opts['args'] or ''
-		if args ~= '' and not args:match('^%d+$') then
-			print('Invalid argument. Please provide a number.')
-			return
-		end
+	local stderr = output.stderr:match("^%s*(.-)%s*$")
+	print(stdout or stderr)
+end
 
-		M.xdgOpenLog(tonumber(args) or 1)
-	end,
-	{
-		nargs = '?',
-	}
-)
+vim.api.nvim_create_user_command("XdgOpenLog", function(maybeOpts)
+	local opts = maybeOpts or {}
+	local args = opts["args"] or ""
+	if args ~= "" and not args:match("^%d+$") then
+		print("Invalid argument. Please provide a number.")
+		return
+	end
+
+	M.xdgOpenLog(tonumber(args) or 1)
+end, {
+	nargs = "?",
+})
 
 return M
